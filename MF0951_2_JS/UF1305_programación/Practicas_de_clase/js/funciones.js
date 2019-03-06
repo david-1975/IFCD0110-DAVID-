8ae279ca97af26eb9b55c187e1e335f44a9f6720
David <davidvalentind@gmail.com>
2019-03-06 14:00:47 +0100

    finclases

---
+++ MF0951_2_JS/UF1305_programación/Practicas_de_clase/js/funciones.js
@@ -0,0 +1,6 @@
+// JavaScript Document
+function miFuncion() {
+document.getElementById('demostracion').innerHTML = "Párrafo y color cambiado."; 
+document.getElementById('demostracion').style.color= "red";	
+}
+
