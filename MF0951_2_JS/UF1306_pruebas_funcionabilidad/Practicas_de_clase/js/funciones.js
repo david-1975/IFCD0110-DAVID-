5d8ba53aff84cccd1a57eaec1b38b0997992109a
David <davidvalentind@gmail.com>
2019-04-03 14:00:32 +0200

    finclass

--- MF0951_2_JS/UF1306_pruebas_funcionabilidad/Practicas_de_clase/js/funciones.js
+++ MF0951_2_JS/UF1306_pruebas_funcionabilidad/Practicas_de_clase/js/funciones.js
@@ -60,17 +60,164 @@ function irAtras() {
 function irAdelante() {
     window.history.forward()
 }
-
-//Mensaje emergente
-function alerta(){
-	windows.alert("Esto es un mensaje de alerta");
+function irA() {
+    window.history.go(-2);
 }
 
-function confirmar(){
-	windows.confirm("Esto es un mensaje de confirmación");
+// MENSAJES EMERGENTES
+function alerta() {
+window.alert("Esto es un mensaje de alerta");
+}
+function confirmar() { 
+window.confirm("Esto es un mensaje de confirmación");
+}
+function introducir() {
+window.prompt("A la espera de órdenes","introduzca su nombre");
 }
 
-function introducir(){
-	windows.prompt("A la espera de órdenes","introduca su nombre");
+// SICRONIZACIÓN
+function myFunction() {
+  var d = new Date();
+  var time = d.toLocaleTimeString();
+	alert("Hora Actual: " + time);
 }
 
+var myVar2;
+function intervalo(){
+  myVar2 = setInterval(myTimer, 3000);
+
+function myTimer() {
+  var d = new Date();
+  var time = d.toLocaleTimeString();
+	alert("Hora Actual: " + time);
+}
+}
+
+function clearintervalo(){
+
+	window.clearInterval(myVar2);
+}
+
+
+//INFORMACIÓN NAVEGADOR
+
+function infoNavegador() {
+
+// MOTOR NAVEGADOR
+var motorNombre = navigator.appName;
+if (motorNombre === "Netscape") {
+	motorNombre = "Javascript";
+	}
+// NOMBRE NAVEGADOR
+var navegador = window.navigator.appCodeName;	
+// VERSION NAVEGADOR
+var versionNav = window.navigator.appVersion;		
+// VERSION NAVEGADOR
+var propietario = "Google. Inc";	
+//LENGUIAJE
+var lenguaje = window.navigator.language;
+//SISTEMA OPERATIVO
+var sistemaOS = "";
+	var agentUser = window.navigator.userAgent;
+	if (agentUser.indexOf("(Windows)")){
+		var sistemaOS = "Windows";
+	}else if (agentUser.indexOf("(Apple)")){
+		var sistemaOS = "Apple";
+	}else if (agentUser.indexOf("(Linux)")){
+		var sistemaOS = "Linux";
+	}else if (agentUser.indexOf("(iPhone)")){
+		var sistemaOS = "iPhone";	
+	}else if (agentUser.indexOf("(ipod)")){
+		var sistemaOS = "ipod";
+	}else if (agentUser.indexOf("(iPad)")){
+		var sistemaOS = "iPad";
+	}else if (agentUser.indexOf("(Adroid)")){
+		var sistemaOS = "Android";
+	}else if (agentUser.indexOf("(otros)")){
+		var sistemaOS = "otros";
+	}
+
+
+//PLATAFORMA
+var plataforma = window.navigator.platform;
+var agentUser = window.navigator.userAgent;
+	
+if ((agentUser.indexOf("Chrome") > -1) ||
+	(agentUser.indexOf("Edge") > -1) ||
+    (agentUser.indexOf("OPR") > -1) || 
+	(agentUser.indexOf("Firefox") > -1)) {
+	var indice7 = agentUser.lastIndexOf("Win64");
+	var plataforma = agentUser.substr(indice7,5); 
+	}
+//	Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36
+else if ((agentUser.indexOf("Safari") > -1) || 
+         (agentUser.indexOf("Trident") > -1)|| 
+		 (agentUser.indexOf("MSIE") > -1)) {
+	var indice8 = agentUser.lastIndexOf("WOW64");
+	var plataforma = "Win64"; 
+	}
+		
+//NAVEGADOR ONLINE
+var navegadorOnline = window.navigator.onLine;
+	if (navegadorOnline === true) {
+	var navegadorOnline = "Si";
+	} else if (navegadorOnline === false) {
+	var navegadorOnline = "No";	
+	} else if (navegadorOnline === undefined) {
+	var navegadorOnline = "Indefinido";	
+	} else if (navegadorOnline === "") {
+	var navegadorOnline = "Desconocido";	
+	}
+	
+var javaActivo = window.navigator.javaEnabled();
+	if (javaActivo === true) {
+	var javaActivo = "Si";
+	} else if (javaActivo === false) {
+	var javaActivo = "No";	
+	} else if (javaActivo === undefined) {
+	var javaActivo = "Indefinido";	
+	} else if (javaActivo === "") {
+	var javaActivo = "Desconocido";	
+	}	
+	
+var cookiesActivas = navigator.cookieEnabled;
+	if (cookiesActivas === true) {
+	var cookiesActivas = "Si";
+	} else if (cookiesActivas === false) {
+	var cookiesActivas = "No";	
+	} else if (cookiesActivas === undefined) {
+	var cookiesActivas = "Indefinido";	
+	} else if (cookiesActivas === "") {
+	var cookiesActivas = "Desconocido";	
+	}	
+	
+var pluginsActivos = window.navigator.plugins.length;
+var agenteUsuario = window.navigator.userAgent;
+
+// MOSTRAR INFO NAVAGADOR
+var txt = "";
+txt += "<h3>Información Navegador</h3>";
+//window.navigator.appName
+txt += "<p><b>Motor Navegador: </b>" + motorNombre + "</p>";
+//window.navigator.appCodeName
+txt += "<p><b>Nombre Navegador: </b>" + navegador + "</p>";	
+//window.navigator.appVersion
+txt += "<p><b>Versión del Navegador: </b>" + versionNav + "</p>"; 
+txt += "<p><b>Propietario: </b>" + propietario + "</p>"; 
+//window.navigator.language;
+txt += "<p><b>Lenguaje: </b>" + lenguaje + "</p>"; 
+//window.navigator.language;
+txt += "<p><b>Sistema Operativo: </b>" + sistemaOS + "</p>"; 
+txt += "<p><b>Plataforma: </b>" + plataforma + "</p>"; 
+//window.navigator.onLine
+txt += "<p><b>Navegador Online: </b>" + navegadorOnline + "</p>"; //window.navigator.javaEnabled()
+txt += "<p><b>Java Activo: </b>" + javaActivo + "</p>"; 
+	//window.navigator.cookieEnabled
+txt += "<p><b>Cookies Activas: </b>" + cookiesActivas + "</p>"; 
+txt += "<p><b>Plugins Activos: </b>" + pluginsActivos + "</p>";
+txt += "<p><b>User-Agent header: </b>" + agenteUsuario + "</p>";// UserAgent
+	
+	
+document.getElementById("infoNav").innerHTML = txt;
+
+}
