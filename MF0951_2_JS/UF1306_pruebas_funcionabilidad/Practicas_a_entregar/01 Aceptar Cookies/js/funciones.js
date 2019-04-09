c3de618fdbe537a077169858df54a345197e0aef
David <davidvalentind@gmail.com>
2019-04-09 13:56:48 +0200

    finclass

--- MF0951_2_JS/UF1306_pruebas_funcionabilidad/Practicas_a_entregar/01 Aceptar Cookies/js/funciones.js
+++ MF0951_2_JS/UF1306_pruebas_funcionabilidad/Practicas_a_entregar/01 Aceptar Cookies/js/funciones.js
@@ -7,40 +7,73 @@
 
 //----------------------
 
-function setCookie(cname,cvalue,exdays) {
-  var d = new Date();
-  d.setTime(d.getTime() + (exdays*24*60*60*1000));
-  var expires = "expires=" + d.toGMTString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+//function setCookie(cname,cvalue,exdays) {
+//  var d = new Date();
+//  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+//  var expires = "expires=" + d.toGMTString();
+//  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+//}
+//
+//function getCookie(cname) {
+//  var name = cname + "=";
+//  var decodedCookie = decodeURIComponent(document.cookie);
+//  var ca = decodedCookie.split(';');
+//  for(var i = 0; i < ca.length; i++) {
+//    var c = ca[i];
+//    while (c.charAt(0) == ' ') {
+//      c = c.substring(1);
+//    }
+//    if (c.indexOf(name) == 0) {
+//      return c.substring(name.length, c.length);
+//    }
+//  }
+//  return "";
+//}
+//
+//function checkCookie() {
+//  var user=getCookie("username");
+//  if (user != "") {
+//    alert("Welcome again " + user);
+//  } else {
+//     user = prompt("Please enter your name:","");
+//     if (user != "" && user != null) {
+//       setCookie("username", user, 30);
+//     }
+//  }
+//	document.getElementById('cookies').style.display = 'none';
+//}
+//var info = document.cookie;
+//alert(info);;
+
+function cerrar(){
+	document.getElementById("micaja").style.display="none";
 }
 
-function getCookie(cname) {
-  var name = cname + "=";
-  var decodedCookie = decodeURIComponent(document.cookie);
-  var ca = decodedCookie.split(';');
-  for(var i = 0; i < ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
-}
 
-function checkCookie() {
-  var user=getCookie("username");
-  if (user != "") {
-    alert("Welcome again " + user);
-  } else {
-     user = prompt("Please enter your name:","");
-     if (user != "" && user != null) {
-       setCookie("username", user, 30);
-     }
-  }
-	document.getElementById('cookies').style.display = 'none';
+
+function funCookies(){
+var verCookie =document.cookie;
+var buscar = verCookie.search("nombre");
+	//alert(buscar);
+	if (buscar !=-1) {
+		alert ("Bienvenido de nuevo a Gourmet de Catering");
+	} else{
+var nombre = "nombre";
+var web = "Gourmet de catering";
+var caduca ="expires";
+var fecha = new Date ();
+var fechaUTC = fecha.toDateString();
+var horaUTC="12:00:00";
+var cookies = nombre + "=" + web + ";" + caduca + "=" + fechaUTC + " " + horaUTC + " UTC; ";
+		
+		//var crearCookie = cookie;
+document.cookie = cookies;
+alert ("Bienvenido a Gourmet de Catering");
+	}
 }
-var info = document.cookie;
-alert(info);
+//var crearcookies ="nombre=Gourmet de catering; expires=Tue 09 Abr 2019 12:00:00 UTC;";
+
+//Añadir cookie
+//
+
+
