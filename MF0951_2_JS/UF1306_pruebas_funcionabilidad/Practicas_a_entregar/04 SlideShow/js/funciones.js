638ec6fe21ccf041b3621c1651bef094cad00cfc
David <davidvalentind@gmail.com>
2019-04-25 13:58:47 +0200

    finclass

--- MF0951_2_JS/UF1306_pruebas_funcionabilidad/Practicas_a_entregar/04 SlideShow/js/funciones.js
+++ MF0951_2_JS/UF1306_pruebas_funcionabilidad/Practicas_a_entregar/04 SlideShow/js/funciones.js
@@ -30,14 +30,57 @@ function showSlides(n) {
 }
 
 //----------------------------------------------
+//Accordion
+var acc = document.getElementsByClassName("accordion");
+var i;
 
-//funciones Tab  Tab Gallery
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
 
-function myFunction(imgs) {
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+//-------------------------------------------
+//TAB
+function openCity(evt, cityName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+
+//-----------------------------------------
+//tab gallery
+function myFunction(imgs){
   // Get the expanded image
   var expandImg = document.getElementById("expandedImg");
   // Get the image text
-  var imgText = document.getElementById("imgtext");
+  var imgText = document.getElementById("imgtxt");
   // Use the same src in the expanded image as the image being clicked on from the grid
   expandImg.src = imgs.src;
   // Use the value of the alt attribute of the clickable image as text inside the expanded image
